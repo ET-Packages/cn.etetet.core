9db85077d05eb9a45d5cdd52a19db995a8b09067
tanghai <egametang@gmail.com>
2025-02-12 17:48:04 +0800

    升级到Unity6

--- Scripts/Core/Share/Helper/ProcessHelper.cs
+++ Scripts/Core/Share/Helper/ProcessHelper.cs
@@ -27,7 +27,7 @@ namespace ET
             }
             else
             {
-                return Run("/usr/local/bin/dotnet", arguments, workingDirectory, waitExit);
+                return Run("/usr/local/share/dotnet/dotnet", arguments, workingDirectory, waitExit);
             }
         }
 
